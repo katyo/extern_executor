8003746683471b838d8135b682064d3b572fb5cc
K <kayo@illumium.org>
2020-04-17 15:49:10 +0500

    Export ffi submodule

--- extern_executor/src/lib.rs
+++ extern_executor/src/lib.rs
@@ -5,13 +5,13 @@ extern crate alloc;
 
 mod types;
 mod userdata;
-mod ffi;
 mod task;
+pub mod ffi;
 
 pub(crate) use types::*;
 pub(crate) use userdata::*;
-pub use ffi::*;
-pub use task::*;
+pub(crate) use task::*;
+pub(crate) use ffi::*;
 
 pub(crate) mod global {
     use super::{UserData, null_mut};
@@ -36,25 +36,26 @@ pub fn spawn(future: impl Future + Send + 'static) {
     task_run(task, task_wrap(future, task));
 }
 
+#[deprecated]
 #[macro_export]
 macro_rules! externs {
     () => {
         /// Initialize async executor by providing task API calls
         #[no_mangle]
-        pub extern "C" fn rust_async_executor_init(task_new: $crate::TaskNew, task_run: $crate::TaskRun, task_wake: $crate::TaskWake, task_data: $crate::ExternData) {
-            $crate::task_init(task_new, task_run, task_wake, task_data);
+        pub extern "C" fn rust_async_executor_init(task_new: $crate::ffi::TaskNew, task_run: $crate::ffi::TaskRun, task_wake: $crate::ffi::TaskWake, task_data: $crate::ffi::ExternData) {
+            $crate::ffi::task_init(task_new, task_run, task_wake, task_data);
         }
 
         /// Task poll function which should be called to resume task
         #[no_mangle]
-        pub extern "C" fn rust_async_executor_poll(task: $crate::InternTask) -> bool {
-            $crate::task_poll(task)
+        pub extern "C" fn rust_async_executor_poll(task: $crate::ffi::InternTask) -> bool {
+            $crate::ffi::task_poll(task)
         }
 
         /// Task drop function which should be called to delete task
         #[no_mangle]
-        pub extern "C" fn rust_aync_executor_drop(task: $crate::InternTask) {
-            $crate::task_drop(task);
+        pub extern "C" fn rust_aync_executor_drop(task: $crate::ffi::InternTask) {
+            $crate::ffi::task_drop(task);
         }
     }
 }
